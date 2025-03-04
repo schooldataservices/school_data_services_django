5e0c48227620dcb57e3c164a3a1dbb4042cceb6c
2015samtaylor <2015samtaylor@gmail.com>
2025-03-03 20:24:35 -0600

    Integrating admin models

--- emailscraper_app/admin.py
+++ emailscraper_app/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 
+from .models import Email, EmailMetadata, RequestConfig   # Replace with your actual model names
+
+admin.site.register(Email)
+admin.site.register(EmailMetadata)
+admin.site.register(RequestConfig)  
